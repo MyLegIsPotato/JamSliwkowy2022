fc9b1c7c92a94a42bc62872be43e99fef3c93502
MyLegIsPotato <12matQ@gmail.com>
2022-07-30 07:25:42 +0200

    backup?

--- Assets/S_PlayerManager.cs
+++ Assets/S_PlayerManager.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class S_PlayerManager : MonoBehaviour
 {
+    public S_UI_Animator UI_Animator;
+    private int selectedWeaponIndex;
+
+    [SerializeField]
+    List<S_Weapon> weapons;
+
     private int playerHealth;
     public int PlayerHealth
     {
@@ -11,4 +17,27 @@ public class S_PlayerManager : MonoBehaviour
         set { playerHealth = value;}
     }
 
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        if(UI_Animator.isPlaying == false){
+            if (Input.mouseScrollDelta == new Vector2(0, 1))
+            {
+                print("weapon Try Up");
+                UI_Animator.WeaponUp();
+                selectedWeaponIndex++;
+
+            }
+            else if (Input.mouseScrollDelta == new Vector2(0, -1))
+            {
+                print("weapon Try Down");
+                UI_Animator.WeaponDown();
+                selectedWeaponIndex--;
+            }
+        }
+    }
 }
--- Assets/S_Weapons/S_Weapon.cs
+++ Assets/S_Weapons/S_Weapon.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class S_Weapon : MonoBehaviour
 {
+    [SerializeField]
     public string weaponName = "Default";
+    [SerializeField]
     public float weaponDamage;
     public float weaponRateOfFire;
     public float weaponReloadTime;
+
 
     public Sprite weaponGraphic;
     public AudioClip weaponSound;
